85715c12597aa4274244af8a34c47910ffea2155
Sergey Galuzov <seregasuper2000@gmail.com>
2021-03-21 20:26:40 +0200

    refactored bmpWriter

--- Writers/BmpWriter.cs
+++ Writers/BmpWriter.cs
@@ -1,54 +1,9 @@
 ﻿using System;
 using System.IO;
+using ImageFormatConverter.FormatData;
 
 namespace ImageFormatConverter.Writers
 {
-    public class BmpHeader
-    {
-        public char[] bitmapSignatureBytes;
-        public int sizeOfBitmapFile;
-        public int reservedBytes;
-        public int pixelDataOffset;
-
-        public BmpHeader(int SizeOfBitmapFileSize)
-        {
-            bitmapSignatureBytes = new []{'B', 'M'};
-            sizeOfBitmapFile = 54 + SizeOfBitmapFileSize;
-            reservedBytes = 0;
-            pixelDataOffset = 14 + 40;
-        }
-    }
-
-    public class BmpInfoHeader
-    {
-        public int sizeOfThisHeader;
-        public int height; // in pixels
-        public int width; // in pixels
-        public ushort numberOfColorPlanes; // must be 1
-        public ushort colorDepth;
-        public int compressionMethod;
-        public uint rawBitmapDataSize; // generally ignored
-        public int horizontalResolution = 3780; // in pixel per meter
-        public int verticalResolution = 3780; // in pixel per meter
-        public uint colorTableEntries = 0;
-        public uint importantColors = 0;
-
-        public BmpInfoHeader(Image image)
-        {
-            sizeOfThisHeader = 40;
-            height = image.Height;
-            width = image.Width;
-            numberOfColorPlanes = 1;
-            colorDepth = 24;
-            compressionMethod = 0;
-            rawBitmapDataSize = 0;
-            horizontalResolution = 3780;
-            verticalResolution = 3780;
-            colorTableEntries = 0;
-            importantColors = 0;
-        }
-    }
-
     public class BmpWriter : ImageWriter
     {
         public BmpWriter(string outputPath) : base(outputPath)
@@ -59,25 +14,24 @@ namespace ImageFormatConverter.Writers
         public override void WriteImage(Image image)
         {
             BinaryWriter writer = new BinaryWriter(new FileStream(OutputPath + ".bmp", FileMode.Create));
-            BmpInfoHeader header2 = new BmpInfoHeader(image);
-            BmpHeader header1 = new BmpHeader(image.Height * image.Width * header2.colorDepth);
-            
-            writer.Write(header1.bitmapSignatureBytes);
-            writer.Write(header1.sizeOfBitmapFile);
-            writer.Write(header1.reservedBytes);
-            writer.Write(header1.pixelDataOffset);
+            BmpMetaData metaData = new BmpMetaData(image);
 
-            writer.Write(header2.sizeOfThisHeader);
-            writer.Write(header2.height);
-            writer.Write(header2.width);
-            writer.Write(header2.numberOfColorPlanes);
-            writer.Write(header2.colorDepth);
-            writer.Write(header2.compressionMethod);
-            writer.Write(header2.rawBitmapDataSize);
-            writer.Write(header2.horizontalResolution);
-            writer.Write(header2.verticalResolution);
-            writer.Write(header2.colorTableEntries);
-            writer.Write(header2.importantColors);
+            writer.Write(BmpMetaData.bitmapSignatureBytes);
+            writer.Write(metaData.sizeOfBitmapFile);
+            writer.Write(BmpMetaData.reservedBytes);
+            writer.Write(BmpMetaData.pixelDataOffset);
+
+            writer.Write(BmpMetaData.sizeOfThisHeader);
+            writer.Write(metaData.height);
+            writer.Write(metaData.width);
+            writer.Write(BmpMetaData.numberOfColorPlanes);
+            writer.Write(BmpMetaData.colorDepth);
+            writer.Write(BmpMetaData.compressionMethod);
+            writer.Write(BmpMetaData.rawBitmapDataSize);
+            writer.Write(BmpMetaData.horizontalResolution);
+            writer.Write(BmpMetaData.verticalResolution);
+            writer.Write(BmpMetaData.colorTableEntries);
+            writer.Write(BmpMetaData.importantColors);
 
             for (int i = image.Height - 1; i >= 0; i--)
             {
--- Writers/FormatData/BmpMetaData.cs
+++ Writers/FormatData/BmpMetaData.cs
@@ -2,6 +2,32 @@ namespace ImageFormatConverter.FormatData
 {
     public class BmpMetaData
     {
+        public static readonly int sizeOfThisHeader = 40;
+        
+        public int height; // in pixels
+        public int width; // in pixels
+        
+        public static readonly ushort numberOfColorPlanes = 1; // must be 1
+        public static readonly ushort colorDepth = 24;
+        public static readonly int compressionMethod = 0;
+        public static readonly uint rawBitmapDataSize = 0; // generally ignored
+        public static readonly int horizontalResolution = 3780; // in pixel per meter
+        public static readonly int verticalResolution = 3780; // in pixel per meter
+        public static readonly uint colorTableEntries = 0;
+        public static readonly uint importantColors = 0;
+        
+        public static readonly char[] bitmapSignatureBytes = {'B', 'M'};
+        public int sizeOfBitmapFile;
+        public static readonly int reservedBytes = 0;
+        public static readonly int pixelDataOffset = 54; //sum of 2 headers' sizes 
+        
+        public BmpMetaData(Image image)
+        {
+            height = image.Height;
+            width = image.Width;
+            int SizeOfBitmapFileSize = image.Height * image.Width * colorDepth;
+            sizeOfBitmapFile = pixelDataOffset + SizeOfBitmapFileSize;
+        }
         
     }
 }
